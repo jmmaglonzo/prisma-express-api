6a9cb9fd75d14e5f0d616bbe724ccbe348dd6a64
jmmaglonzo <jmmaglonzo.dev@gmail.com>
2024-09-27 01:54:19 +0800

    feat(utils): created asynchandler and custom error class

---
+++ src/utils/async-handler.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+const asyncHandler = (fn: Function) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default asyncHandler;
